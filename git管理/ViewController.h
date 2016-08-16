2bd51d1523a79ec1992d099a5763d637460ed239
denny <denny@ugens-macbook.local>
2016-08-16 09:48:33 +0800

    Initial Commit

---
+++ git管理/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git管理
+//
+//  Created by denny on 16/8/16.
+//  Copyright © 2016年 denny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ git管理/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  git管理
+//
+//  Created by denny on 16/8/16.
+//  Copyright © 2016年 denny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
